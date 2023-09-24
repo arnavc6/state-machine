b71e593feceb0825b8ec526e683649898ce41e38
arnavc6 <arnavchokshi172@gmail.com>
2023-09-24 19:39:35 -0400

    added values to enum and made a constant loop

--- pedestrian/src/main.rs
+++ pedestrian/src/main.rs
@@ -3,17 +3,34 @@ use std::collections::HashMap;
 
 #[derive(PartialEq)]
 enum TrafficLight{
-    Red,
-    Yellow,
     Green,
+    Yellow,
+    Red
 }
 
 #[derive(PartialEq)]
 enum CrossWalkSign{
-    Walk,
-    Countdown,
-    Stop,
+    Ready,
+    Waiting(u64),
+    Walk(u64),
+    Countdown(u64),
+    Blocked(u64)
 }
+
+static WAIT_TIME: u64 = 8;
+static CROSS_TIME: u64 = 20;
+static BLOCK_TIME: u64 = 20;
+
+/**
+ * 1. Green/Ready
+ * 2. Yellow/Waiting
+ * 3. Red/Waiting
+ * 4. Red/Walk
+ * 5. Red/Countdown
+ * 6. Red/Blocked
+ * 7. Green/Blocked
+ * loop back
+ */
 
 struct Intersection{
     light: TrafficLight,
@@ -26,45 +43,79 @@ enum IntersectionState{
     Stopped,
 }
 
-fn changeState(intersection: &mut Intersection) {
-    intersection.light = TrafficLight::Yellow;
-    println!("Light is yellow, crosswalk is stopped");
-    thread::sleep(time::Duration::from_secs(5));
-    
-    intersection.light = TrafficLight::Red;
-    println!("Light is red, crosswalk is stopped");
-    thread::sleep(time::Duration::from_secs(2));
-    
-    intersection.crosswalk = CrossWalkSign::Walk; 
-    println!("Light is red, crosswalk is on");
-    thread::sleep(time::Duration::from_secs(10));
-    intersection.crosswalk = CrossWalkSign::Countdown; 
-    println!("Light is red, crosswalk is counting down");
-    thread::sleep(time::Duration::from_secs(10));    
-    intersection.crosswalk = CrossWalkSign::Stop;
-    println!("Light is red, crosswalk is stopped");
-    thread::sleep(time::Duration::from_secs(2));
+fn change_state(intersection: &mut Intersection) {
+    intersection.crosswalk = CrossWalkSign::Waiting(WAIT_TIME);
+    match intersection.crosswalk{
+        CrossWalkSign::Waiting(x) => {
+            intersection.light = TrafficLight::Yellow;
+            println!("Light is yellow, {} seconds until crossing", x);
+            thread::sleep(time::Duration::from_secs(x/2));
+            intersection.light = TrafficLight::Red;
+            println!("Light is red, {} seconds until crossing", x/2);
+            thread::sleep(time::Duration::from_secs(x/2));
+        }
+        _ => {
+            println!("crosswalk machine 🅱️roke")
+        }
+    }
 
-    intersection.light = TrafficLight::Green;
-    println!("Light is green, crosswalk is stopped. Crossing is not available for 20 seconds");
-    thread::sleep(time::Duration::from_secs(20));
+    intersection.crosswalk = CrossWalkSign::Walk(CROSS_TIME);
+    match intersection.crosswalk{
+        CrossWalkSign::Walk(x) => {
+            println!("Walk sign is on to cross, {} seconds remaining", x);
+            thread::sleep(time::Duration::from_secs(x/2));
+        }
+        _ => {
+            println!("crosswalk machine 🅱️roke")
+        }
+    }
+
+    intersection.crosswalk = CrossWalkSign::Countdown(CROSS_TIME/2);
+    match intersection.crosswalk{
+        CrossWalkSign::Countdown(x) => {
+            let mut y = x;
+            while y > 0{
+                println!("Counting down... {} seconds remaining", y);
+                thread::sleep(time::Duration::from_secs(1));
+                y -= 1;
+            }
+        }
+        _ => {
+            println!("crosswalk machine 🅱️roke")
+        }
+    }
+    
+    intersection.crosswalk = CrossWalkSign::Blocked(BLOCK_TIME);
+    match intersection.crosswalk{
+        CrossWalkSign::Blocked(x) => {
+            println!("Crossing will be available in {} seconds", x);
+            thread::sleep(time::Duration::from_secs(x));
+        }
+        _ => {
+            println!("crosswalk machine 🅱️roke")
+        }
+    }
 }
 
 
 fn main() {
     let mut intersection = Intersection{
         light: TrafficLight::Green,
-        crosswalk: CrossWalkSign::Stop,
+        crosswalk: CrossWalkSign::Ready,
     };
-    println!("Traffic light is green, press a key to cross road");
-
-    // User input to cross
-    let mut line = String::new();
-    let b1 = std::io::stdin().read_line(&mut line).unwrap();
     
-    if b1 > 0 {
-        // call change state function
-        changeState(&mut intersection);
-    }
+    while true{
+        println!("Traffic light is green, press a key to cross road or hit enter to exit");
 
+        // User input to cross
+        let mut line = String::new();
+        let b1 = std::io::stdin().read_line(&mut line).unwrap();
+        
+        if b1 > 1 {
+            // call change state function
+            change_state(&mut intersection);
+        } else{
+            break;
+        }
+    }
 }
